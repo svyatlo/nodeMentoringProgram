698aeb8eea2fb312abfc5320d1b64285907c968d
svyatlo <svyatlo@list.ru>
2019-12-03 01:23:10 +0300

    rewrite functions using babel and ES6 modules

---
+++ src/task1/task1-3/loadTextByChunks.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const csv = require('csvtojson');
+const { pipeline } = require('stream');
+
+export default function loadTextByChunks(sourcePath, targetPath) {
+  pipeline(
+    fs.createReadStream(sourcePath),
+    csv(),
+    fs.createWriteStream(targetPath, 'utf8'),
+    (error) => {
+      if (error) {
+        console.error('Pipeline failed.', error.message);
+      } else {
+        console.log('Pipeline succeeded.');
+      }
+    }
+  );
+}
