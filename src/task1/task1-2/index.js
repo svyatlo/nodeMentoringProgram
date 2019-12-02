fa5dab4776785d5a249063932486d736fd06ff8d
Sviatlana Tamashevich <sviatlana.tamashevich@thomsonreuters.com>
2019-12-02 15:28:35 +0300

    refactor task1-2/index.js

--- src/task1/task1-2/index.js
+++ src/task1/task1-2/index.js
@@ -3,29 +3,40 @@ const fs = require('fs');
 const { pipeline } = require('stream');
 const csv = require('csvtojson');
 
-const csvFilePath = path.join(__dirname, 'csv/node_mentoring_t1_2_input_example.csv');
+const csvFilePath = path.join(__dirname, '../csv/node_mentoring_t1_2_input_example.csv');
 
-csv()
-.fromFile(csvFilePath)
-.then(jsonFormatData => {
-  console.log('\nThe file is loaded fully into the RAM');
-  const stringFormatData = jsonFormatData.map(item => JSON.stringify(item)).join('\n');
-
-  fs.writeFile(path.join(__dirname, 'loaded_fully_text.txt'), stringFormatData, (error) => {
-    if (error) throw error;
-    console.log('The file has been saved!');
+function loadTextFully() {
+  csv()
+  .fromFile(csvFilePath)
+  .then(jsonFormatData => {
+    console.log('\nThe file is loaded fully into the RAM');
+    const stringFormatData = jsonFormatData.map(item => JSON.stringify(item)).join('\n');
+  
+    fs.writeFile(path.join(__dirname, 'text_is_fully_loaded.txt'), stringFormatData, (error) => {
+      if (error) {
+        console.error(error.message);
+      }
+      console.log('The file has been saved!');
+    });
   });
-});
+}
 
-pipeline(
-  fs.createReadStream(csvFilePath),
-  csv(),
-  fs.createWriteStream(path.join(__dirname, 'loaded_text_by_chunks.txt')),
-  (err) => {
-    if (err) {
-      console.error('Pipeline failed.', err);
-    } else {
-      console.log('Pipeline succeeded.');
+function loadTextByChunks() {
+  pipeline(
+    fs.createReadStream(csvFilePath),
+    csv(),
+    fs.createWriteStream(path.join(__dirname, 'text_is_loaded_by_chunks.txt'), 'utf8'),
+    (error) => {
+      if (error) {
+        console.error('Pipeline failed.', error.message);
+      } else {
+        console.log('Pipeline succeeded.');
+      }
     }
-  }
-);
+  );
+}
+
+
+loadTextFully();
+loadTextByChunks();
+
