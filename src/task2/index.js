7d60f0ed668ef0bdf48a0dda37e6e9ab0a2453c4
Sviatlana Tamashevich <sviatlana.tamashevich@thomsonreuters.com>
2019-12-13 18:22:26 +0300

    task2: add method GET REST service

--- src/task2/index.js
+++ src/task2/index.js
@@ -1,10 +1,14 @@
-import persons from './db';
+import db from './db';
 const express = require('express');
 const app = express();
 const port = 3001;
 
-app.get('/', (req, res) => {
-    res.send(`Hello world from ${JSON.stringify(persons[0])}`);
+app.get('/persons', (req, res) => {
+    res.status(200).send({
+        success: 'true',
+        message: 'persons retrieved successfully',
+        persons: db
+    });
 });
 
 app.listen(port, () => {
