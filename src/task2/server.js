ce92cd6e78f4b1ac90206046ae881e2d7de3ba3a
svyatlo <svyatlo@list.ru>
2019-12-17 01:10:06 +0300

    task2: refactor server.js, add validation

--- src/task2/server.js
+++ src/task2/server.js
@@ -1,35 +1,23 @@
 import persons from './exampleDB';
-import { get } from './api/get';
-import { getById } from './api/getById';
-import { updateById } from './api/updateById';
-import { post } from './api/post';
-import { deleteById } from './api/deleteById';
+import { routs } from './routs/routs';
+import { schema } from './routs/validationSchema';
 
 const express = require('express');
 const app = express();
-const Joi = require('joi');
 const validator = require('express-joi-validation').createValidator({});
-const PORT = process.env.PORT || 5000;
+const port = 5000;
 
-const querySchema = Joi.object({
-    id: Joi.required(),
-    login: Joi.string().required(),
-    password: Joi.string().required(),
-    age: Joi.string().required(),
-    isDeleted: Joi.boolean().required()
-});
-
-app.set('port', PORT);
+app.set('port', port);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/persons', validator.query(querySchema), (req, res) => get(req, res, persons));
-app.get('/persons/:id', (req, res) => getById(req, res, persons));
-app.put('/persons/:id', (req, res) => updateById(req, res, persons));
-app.post('/persons', (req, res) => post(req, res, persons));
-app.delete('/persons/:id', (req, res) => deleteById(req, res, persons));
+app.get('/persons', (req, res) => routs.get(req, res, persons));
+app.get('/persons/:id', validator.params(schema.idSchema), (req, res) => routs.getById(req, res, persons));
+app.put('/persons/:id', validator.params(schema.idSchema), (req, res) => routs.updateById(req, res, persons));
+app.post('/persons', validator.body(schema.objectSchema), (req, res) => routs.post(req, res, persons));
+app.delete('/persons/:id', validator.params(schema.idSchema), (req, res) => routs.deleteById(req, res, persons));
 
-app.listen(PORT, () => {
+app.listen(port, () => {
     console.log(`server started at http:/localhost: ${app.get('port')}`);
 });
