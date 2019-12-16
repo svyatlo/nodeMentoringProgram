ee2c6923c72146cbc744874bb89f6ac4cac47ff3
Sviatlana Tamashevich <sviatlana.tamashevich@thomsonreuters.com>
2019-12-16 18:17:20 +0300

    task2: add validator

--- src/task2/server.js
+++ src/task2/server.js
@@ -4,16 +4,27 @@ import { getById } from './api/getById';
 import { updateById } from './api/updateById';
 import { post } from './api/post';
 import { deleteById } from './api/deleteById';
+
 const express = require('express');
 const app = express();
+const Joi = require('joi');
+const validator = require('express-joi-validation').createValidator({});
 const PORT = process.env.PORT || 5000;
+
+const querySchema = Joi.object({
+    id: Joi.required(),
+    login: Joi.string().required(),
+    password: Joi.string().required(),
+    age: Joi.string().required(),
+    isDeleted: Joi.boolean().required()
+});
 
 app.set('port', PORT);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/persons', (req, res) => get(req, res, persons));
+app.get('/persons', validator.query(querySchema), (req, res) => get(req, res, persons));
 app.get('/persons/:id', (req, res) => getById(req, res, persons));
 app.put('/persons/:id', (req, res) => updateById(req, res, persons));
 app.post('/persons', (req, res) => post(req, res, persons));
