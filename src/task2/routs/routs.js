bcc74d3e66c60f29046a6c9b5b0b1ecebf2ff9e5
svyatlo <svyatlo@list.ru>
2019-12-16 23:32:25 +0300

    task2: add routs.js

---
+++ src/task2/routs/routs.js
@@ -0,0 +1,13 @@
+import { get } from './get';
+import { getById } from './getById';
+import { updateById } from './updateById';
+import { post } from './post';
+import { deleteById } from './deleteById';
+
+export const routs = {
+  get,
+  getById,
+  updateById,
+  post,
+  deleteById
+}
