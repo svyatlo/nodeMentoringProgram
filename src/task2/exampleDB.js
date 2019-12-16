225f9c43d555d3c07871ad7f05f4552248888a90
Sviatlana Tamashevich <sviatlana.tamashevich@thomsonreuters.com>
2019-12-16 14:51:48 +0300

    task2: change example database file

---
+++ src/task2/exampleDB.js
@@ -0,0 +1,30 @@
+module.exports = [
+    {
+        id: '1982c440-8318-48ed-955e-7d7b193eaf09',
+        login: 'svyatlo',
+        password: 'abc123456',
+        age: 101,
+        isDeleted: false
+    },
+    {
+        id: '38aec697-9af1-4980-b664-dcf9e5eb7cda',
+        login: 'oltayvs',
+        password: 'cba654321',
+        age: 102,
+        isDeleted: false
+    },
+    {
+        id: '38aec697-9af1-5613-b664-dcf9e5ev6cda',
+        login: 'temno',
+        password: 'dfg123456',
+        age: 89,
+        isDeleted: false
+    },
+    {
+        id: '38nmy697-9af1-4980-b685-dcf9e5eb7cda',
+        login: 'onmet',
+        password: 'gfd654321',
+        age: 98,
+        isDeleted: false
+    }
+];
