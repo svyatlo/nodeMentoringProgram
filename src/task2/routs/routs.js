989d64721ae04f1d9f432be6ba0a989c72bd455e
svyatlo <svyatlo@list.ru>
2019-12-18 00:54:25 +0300

    task2: implement express.Router

---
+++ src/task2/routs/routs.js
@@ -0,0 +1,28 @@
+import { persons } from '../exampleDB';
+import { routsHandlers } from '../api/routsHandlers';
+import { schema } from '../api/validationSchema';
+
+const express = require('express');
+const router = express.Router();
+const validator = require('express-joi-validation').createValidator({});
+
+router.route('/persons')
+.get((req, res) => {
+  validator.response(schema.arraySchema);
+  routsHandlers.get(req, res, persons);
+})
+.post((req, res) => {
+  validator.body(schema.objectSchema);
+  routsHandlers.post(req, res, persons);
+})
+
+router.route('/persons/:id')
+.all((req, res, next) => {
+  validator.params(schema.idSchema);
+  next()
+})
+.get((req, res) => routsHandlers.getById(req, res, persons))
+.put((req, res) => routsHandlers.updateById(req, res, persons))
+.delete((req, res) => routsHandlers.deleteById(req, res, persons))
+
+export default router;
--- src/task2/server.js
+++ src/task2/server.js
@@ -1,22 +1,14 @@
-import persons from './exampleDB';
-import { routs } from './api/routs';
-import { schema } from './api/validationSchema';
+import router from './routs/routs';
 
 const express = require('express');
 const app = express();
-const validator = require('express-joi-validation').createValidator({});
 const port = 5000;
 
 app.set('port', port);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.get('/persons', validator.response(schema.arraySchema), (req, res) => routs.get(req, res, persons));
-app.get('/persons/:id', validator.params(schema.idSchema), (req, res) => routs.getById(req, res, persons));
-app.put('/persons/:id', validator.params(schema.idSchema), (req, res) => routs.updateById(req, res, persons));
-app.post('/persons', validator.body(schema.objectSchema), (req, res) => routs.post(req, res, persons));
-app.delete('/persons/:id', validator.params(schema.idSchema), (req, res) => routs.deleteById(req, res, persons));
+app.use(router);
 
 app.listen(port, () => {
     console.log(`server started at http:/localhost: ${app.get('port')}`);
