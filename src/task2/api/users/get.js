f0a24b1b0273a141c9cee1d3ada1a5b668dacf6f
svyatlo <svyatlo@list.ru>
2020-01-23 00:14:05 +0300

    Task2: refactor routes handlers

--- src/task2/api/users/get.js
+++
@@ -1,9 +0,0 @@
-import { findByLogin } from './findByLogin';
-
-export function get(req, res, database) {
-    if (req.query.loginSubstring && req.query.limit) {
-        return findByLogin(req, res);
-    }
-
-    return res.status(200).send(database);
-}
